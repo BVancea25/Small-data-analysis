421169bdd2bb23e09c2cf2122228eeb7d84644c3
BVancea25 <b_vancea@yahoo.com>
2024-04-10 23:44:08 +0300

    first

---
+++ src/main/java/com/example/proiectsh/ProiectShApplication.java
@@ -0,0 +1,13 @@
+package com.example.proiectsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectShApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectShApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/proiectsh/Repository/DataRepository.java
@@ -0,0 +1,8 @@
+package com.example.proiectsh.Repository;
+
+import com.example.proiectsh.Models.Data;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DataRepository extends JpaRepository<Data, Long> {
+
+}
